cdf352580918eac98ad03206d8d42dd9c567d294
An Qi Ke <kea@bxscience.edu>
2024-02-21 11:56:42 -0500

    commented out commands

--- src/main/java/frc/robot/commands/SpinShooterUp.java
+++ src/main/java/frc/robot/commands/SpinShooterUp.java
@@ -1,32 +1,32 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.ShooterWheel;
-import frc.robot.Constants.ShooterWheelConstants;
-import frc.robot.subsystems.ShooterAngle;
+// import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+// import frc.robot.subsystems.ShooterWheel;
+// import frc.robot.Constants.ShooterWheelConstants;
+// import frc.robot.subsystems.ShooterAngle;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class SpinShooterUp extends SequentialCommandGroup {
-  private final ShooterWheel shooterWheel;
-  private final ShooterAngle shooterAngle;
+// // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// // information, see:
+// // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+// public class SpinShooterUp extends SequentialCommandGroup {
+//   private final ShooterWheel shooterWheel;
+//   private final ShooterAngle shooterAngle;
 
-  /** Creates a new SpinShooterUp. */
-  public SpinShooterUp(ShooterWheel wheel, ShooterAngle angle) {
-    shooterWheel = wheel;
-    shooterAngle = angle;
+//   /** Creates a new SpinShooterUp. */
+//   public SpinShooterUp(ShooterWheel wheel, ShooterAngle angle) {
+//     shooterWheel = wheel;
+//     shooterAngle = angle;
 
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(
-        new ParallelCommandGroup(
-            new ShooterVelocity(shooterWheel, ShooterWheelConstants.SHOOTER_METERS_SECOND),
-            new SetShooterAngle(shooterAngle, 60)));
-  }
-}
+//     // Add your commands in the addCommands() call, e.g.
+//     // addCommands(new FooCommand(), new BarCommand());
+//     addCommands(
+//         new ParallelCommandGroup(
+//             new ShooterVelocity(shooterWheel, ShooterWheelConstants.SHOOTER_METERS_SECOND),
+//             new SetShooterAngle(shooterAngle, 60)));
+//   }
+// }

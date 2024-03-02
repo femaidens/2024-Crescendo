2b6cad53e468dbf98618ee262fc5618f1b3e6a98
Olivia Wang <oliviawang@Olivias-MacBook-Air.local>
2024-03-02 09:49:48 -0500

    edited beam break code

--- src/main/java/frc/robot/subsystems/BeamBreak.java
+++ src/main/java/frc/robot/subsystems/BeamBreak.java
@@ -4,22 +4,28 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Ports.BeamBreakPorts;
 
 // import frc.robot.Ports.BeamBreakPorts;
 
 public class BeamBreak extends SubsystemBase {
-
   /** Creates a new BeamBreak. */
-  // private final DigitalInput receiver;
-  // private final DigitalOutput emitter;
+  private final DigitalInput receiver;
+  private final DigitalOutput emitter;
+  private final boolean isReceived;
+  private final Debouncer debouncer;
 
   public BeamBreak() {
-    // receiver = new DigitalInput(BeamBreakPorts.RECEIVER);
-    // emitter = new DigitalOutput(BeamBreakPorts.EMITTER);
+    receiver = new DigitalInput(BeamBreakPorts.RECEIVER);
+    emitter = new DigitalOutput(BeamBreakPorts.EMITTER);
+    debouncer = new Debouncer(0.1, DebounceType.kBoth);
+    isReceived = receiver.get();
     // setEmitter(true); // added it as default command in robot container
   }
 
@@ -36,14 +42,25 @@ public class BeamBreak extends SubsystemBase {
   // public void setEmitter(boolean status) {
   //   emitter.set(status);
   // }
+  
+  public void read(){
+    if(receiver.get()){
+      // motor.set(0.2);
+      System.out.println("working");
+    } else {
+      System.out.println("off");
+    }
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     // System.out.println("receiver status: " + getReceiverStatus());
     // System.out.println("emitter status: " + getEmitterStatus());
-
+    
     // SmartDashboard.putBoolean("receiver status", getReceiverStatus());
     // SmartDashboard.putBoolean("emitter status", getEmitterStatus());
+
+    SmartDashboard.putBoolean("receiver", debouncer.calculate(receiver.get()));
   }
 }

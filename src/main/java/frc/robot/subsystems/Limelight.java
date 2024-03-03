cc6c898aae387f69d63f16e4c2fb01a1b2586c23
An Qi Ke <kea@bxscience.edu>
2024-03-02 22:53:04 -0500

    added comment

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
+  // main limelight code
   /** Creates a new Limelight. */
   public Limelight() {}
 

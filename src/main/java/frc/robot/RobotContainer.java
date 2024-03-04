a3869c65d8fb808febefeccdadafa8cc70de8e6f
Olivia Wang <oliviawang@Olivias-MacBook-Air.local>
2024-03-04 16:36:11 -0500

    added limelight

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import frc.robot.subsystems.Climb;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.ShooterAngle;
 import frc.robot.subsystems.ShooterWheel;
 
@@ -39,7 +40,7 @@ public class RobotContainer {
   private final ShooterWheel shooterWheel = new ShooterWheel();
   private final ShooterAngle shooterAngle = new ShooterAngle();
   private final Climb climb = new Climb();
-
+  private final Limelight limelight = new Limelight();
   private final Shooter shooter = new Shooter(shooterAngle, shooterWheel, hopper);
   private final Intaking intaking = new Intaking(intake, hopper);
   private final Controls controls = new Controls(shooterAngle, shooterWheel, hopper, intake, drivetrain);
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,14 +4,35 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
+  private NetworkTable table;
+  private NetworkTableEntry tv;
+  private NetworkTableEntry tx;
+  private NetworkTableEntry ta;
+  private NetworkTableEntry ts;
+  private NetworkTableEntry ty;
   /** Creates a new Limelight. */
-  public Limelight() {}
+  public Limelight() {
+    table = NetworkTableInstance.getDefault().getTable("Limelight");
+    tv = table.getEntry("tv");
+    tx = table.getEntry("tx");
+    ta = table.getEntry("ta");
+    ty = table.getEntry("ty");
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    
+    SmartDashboard.putNumber("tv", tv.getDouble(0.0));
+    SmartDashboard.putNumber("tx", tx.getDouble(0.0));
+    SmartDashboard.putNumber("ta", ta.getDouble(0.0));
+    SmartDashboard.putNumber("ty", ty.getDouble(0.0));
   }
 }

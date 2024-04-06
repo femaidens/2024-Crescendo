b2af159d5abcf07bf729594b62544f04debf00eb
yujaime <yuj@bxscience.edu>
2024-04-06 00:54:11 -0400

    added two note corner blue right; two note corner blue left

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,6 +148,7 @@ public class RobotContainer implements Logged {
     NamedCommands.registerCommand("ampshoot", shooter.shoot(ShooterAngleConstants.AMP_FLUSH, ShooterWheelConstants.AMP_FLUSH));
     NamedCommands.registerCommand("speakerstageshoot", shooter.shoot(ShooterAngleConstants.SPEAKER_STAGE, ShooterWheelConstants.SPEAKER_STAGE));
     NamedCommands.registerCommand("intake", intaking.intakeNote());
+    NamedCommands.registerCommand("speakerflushshoot", shooter.shoot(ShooterAngleConstants.SPEAKER_FLUSH, ShooterWheelConstants.SPEAKER_FLUSH));
 
     // AutoBuilder autoBuilder = new AutoBuilder();
         AutoBuilder.configureHolonomic(

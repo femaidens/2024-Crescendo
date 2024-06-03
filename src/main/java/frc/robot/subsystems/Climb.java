4703b0911388a734c1fa088df3b447e49f244fc2
Ngozi Ihesiaba <ihesiaban@bxscience.edu>
2024-06-03 16:51:56 -0400

    Commandified the climb module

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -10,6 +10,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.*;
 import frc.robot.Ports.*;
@@ -44,32 +45,33 @@ public class Climb extends SubsystemBase implements Logged {
     return this.runOnce(() -> stopMotors());
   }
 
-  public void extendClimbArm() {
+  /**
+   * Extends arm until bottom limit switch is activated
+   * 
+   */
+  public Command extendClimbArm() {
     if(isBotActivated()){ // stop extending if bottom hits top
-      rightArm.set(0);
-      leftArm.set(0);
       System.out.println("bottom limit switch hit!");
+      return this.run(() -> stopMotors())
+      .alongWith(new PrintCommand("Stopped Arm"));
     }
 
     else {
-      rightArm.set(ClimberConstants.ARM_SPEED);
-      leftArm.set(-ClimberConstants.ARM_SPEED);
-      System.out.println("extending arm");
+      return this.run(() -> setSpeed(ClimberConstants.ARM_SPEED))
+      .alongWith(new PrintCommand("Extending Arm"));
     }
   }
 
   public void retractClimbArm() {
     if(isTopActivated()){ // stop retracting if top hits bottom
-      rightArm.set(0);
-      leftArm.set(0);
+      this.run(() -> stopMotors());
       System.out.println("top switch activated!");
     }
 
     else {
       // check inversion of motors
-      rightArm.set(-ClimberConstants.ARM_SPEED);
-      leftArm.set(ClimberConstants.ARM_SPEED);
       System.out.println("retracting arm");
+      this.run(() -> setSpeed(-ClimberConstants.ARM_SPEED));
     }
     
   }
@@ -87,6 +89,14 @@ public class Climb extends SubsystemBase implements Logged {
     leftArm.set(0);
   }
 
+  /**
+   * Sets the speed of both climb motors
+   * @param speed PWM value
+   */
+  public void setSpeed(double speed){  //positive up, negative down
+    rightArm.set(speed);
+    leftArm.set(-speed);
+  }
   @Override
   public void periodic() {
     SmartDashboard.putBoolean("top limit switch", isTopActivated());
